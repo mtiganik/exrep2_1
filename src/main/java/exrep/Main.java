a420b5c59a90136f12065b5a59f9330f407efda3
Märt <mart.kalmo@gmail.com>
2014-09-13 09:46:59 +0300

    fib format fix

--- src/main/java/exrep/Main.java
+++ src/main/java/exrep/Main.java
@@ -6,51 +6,51 @@ import org.junit.Test;
 
 public class Main {
 
-	@Test
-	public void ex1() {
-		// new Person("Jack");
-		// Util.printHello();
-		// printHello();
-	}
+    @Test
+    public void ex1() {
+        // new Person("Jack");
+        // Util.printHello();
+        // printHello();
+    }
 
-	@Test
-	public void ex2() {
-		new Company();
-		new Company();
-	}
+    @Test
+    public void ex2() {
+        new Company();
+        new Company();
+    }
 
-	@Test
-	public void ex3() {
-		try {
-			System.out.println("in try block");
-			// throw new RuntimeException();
-			// return;
-			
-		} catch (Exception e) {
-			System.out.println("Catched: " + e);
-		} finally {
-			System.out.println("in finally block");
-		}
-	}
+    @Test
+    public void ex3() {
+        try {
+            System.out.println("in try block");
+            // throw new RuntimeException();
+            // return;
 
-	@Test
-	public void ex4() {
-		// Util.readFromFile();
-		
-		// Util.readFromWeb();
-	}
+        } catch (Exception e) {
+            System.out.println("Catched: " + e);
+        } finally {
+            System.out.println("in finally block");
+        }
+    }
 
-	@Test
-	public void ex5() {
-		System.out.println(fib(7));
-	}
+    @Test
+    public void ex4() {
+        // Util.readFromFile();
 
-	private Map<Integer, Integer> cache = new HashMap<Integer, Integer>();
-	
-	private int fib(int n) {
-		System.out.println("n = " + n);
+        // Util.readFromWeb();
+    }
 
-		return 0;
-	}
-	
+    @Test
+    public void ex5() {
+        System.out.println(fib(7));
+    }
+
+    private Map<Integer, Integer> cache = new HashMap<Integer, Integer>();
+
+    private int fib(int n) {
+        System.out.println("fib(" + n + ")");
+
+        return 0;
+    }
+
 }
